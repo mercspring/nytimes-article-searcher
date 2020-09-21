9b99f2491d92e652c0611c43b44d0de4bdadca9a
Jae McLain <jae.mclain@gmail.com>
2020-09-21 14:10:33 -0700

    testing

---
+++ script.js
@@ -0,0 +1,8 @@
+// Button - On click
+$('#runSearch').on('click', function(){
+
+    // Search - Input
+    var searchTerm = $('#searchTerm').val().trim();
+    queryURL = queryURLBase + searchTerm
+
+})
